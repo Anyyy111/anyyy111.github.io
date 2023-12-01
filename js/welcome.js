6b12f63b67adcf1ebeab346126997137efc31479
Anyyy <1395980247@qq.com>
2023-12-02 01:14:19 +0800

    Site updated: 2023-12-02 01:14:19

--- js/welcome.js
+++ js/welcome.js
@@ -5,18 +5,18 @@ function welcome(){
         welcome_text="欢迎你，来自"+referrer.toUpperCase()+"的用户！";
         if(referrer.toUpperCase()==document.domain.toUpperCase())return;
     }
-    swal({
-        title: "图片无法加载的情况",
-        type: "info",
-        text: '<p>请使用<span style="color: green">火狐浏览器</span>打开本站体验最佳;</br><span style="color: red">谷歌、Edge浏览器</span>请参考文章: <a href="https://www.anyiblog.top/inform/">https://www.anyiblog.top/inform/</a></p>',
-        html: true,
-        imageUrl: "",
-        timer: 5000,
-        showConfirmButton: true,
-        allowOutsideClick: true,
-        confirmButtonColor: "black",
-        confirmButtonText: '我知道啦',
-    });
+    // swal({
+    //     title: "图片无法加载的情况",
+    //     type: "info",
+    //     text: '<p>请使用<span style="color: green">火狐浏览器</span>打开本站体验最佳;</br><span style="color: red">谷歌、Edge浏览器</span>请参考文章: <a href="https://www.anyiblog.top/inform/">https://www.anyiblog.top/inform/</a></p>',
+    //     html: true,
+    //     imageUrl: "",
+    //     timer: 1000,
+    //     showConfirmButton: true,
+    //     allowOutsideClick: true,
+    //     confirmButtonColor: "black",
+    //     confirmButtonText: '我知道啦',
+    // });
 }
 $(document).ready(()=>{
     welcome()
